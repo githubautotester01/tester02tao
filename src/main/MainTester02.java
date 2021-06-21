71df08181f3b256a40f1ea86c594cbfe50838072
TMHoang <github.autotester02@gmail.com>
2021-06-21 23:30:09 +0700

    demo fork

--- src/main/MainTester02.java
+++ src/main/MainTester02.java
@@ -3,6 +3,6 @@ package main;
 public class MainTester02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is tester 02 !!! update nha");
+		System.out.println("This is tester 02 !!! update nha , fork from 02");
 	}
 }
